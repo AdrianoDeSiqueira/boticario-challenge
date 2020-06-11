60806b846b4a9c1f2f1e402101a63680b17f346c
AdrianoDeSiqueira <adrianodizao@hotmail.com>
2020-06-11 11:11:39 -0300

    test: ensure DbAddReseller calls Hasher with correct password

---
+++ src/data/usecases/reseller/db-add-reseller.spec.ts
@@ -0,0 +1,41 @@
+import { DbAddReseller } from './db-add-reseller'
+import { AddResellerModel, Hasher } from './db-add-reseller-protocols'
+
+const makeHasher = (): Hasher => {
+  class HasherStub implements Hasher {
+    async hash (value: string): Promise<string> {
+      return Promise.resolve('any_password_hashed')
+    }
+  }
+  return new HasherStub()
+}
+
+const makeFakeResellerData = (): AddResellerModel => ({
+  socialSecurityNumber: 'any_social_security_number',
+  name: 'any_name',
+  email: 'any_email@mail.com',
+  password: 'any_password'
+})
+
+interface sutTypes {
+  sut: DbAddReseller
+  hasherStub: Hasher
+}
+
+const makeSut = (): sutTypes => {
+  const hasherStub = makeHasher()
+  const sut = new DbAddReseller(hasherStub)
+  return {
+    sut,
+    hasherStub
+  }
+}
+
+describe('DbAddReseller Usecase', () => {
+  test('Should call Hasher with correct password', async () => {
+    const { sut, hasherStub } = makeSut()
+    const hashSpy = jest.spyOn(hasherStub, 'hash')
+    await sut.add(makeFakeResellerData())
+    expect(hashSpy).toHaveBeenCalledWith('any_password')
+  })
+})
