8531081b19772ffdfbe1d8d35ac89e6e8b2c9b66
AdrianoDeSiqueira <adrianodizao@hotmail.com>
2020-06-11 01:35:44 -0300

    feat: ensure AddResellerController returns 500 if AddReseller returns throws

--- src/presentation/controllers/reseller/add-reseller/add-reseller-controller.ts
+++ src/presentation/controllers/reseller/add-reseller/add-reseller-controller.ts
@@ -1,5 +1,5 @@
 import { HttpRequest, HttpResponse, Controller, Validation } from './add-reseller-controller-protocols'
-import { badRequest } from '../../../helpers/http/http-helper'
+import { badRequest, serverError } from '../../../helpers/http/http-helper'
 import { AddReseller } from '../../../../domain/usecases/reseller/add-reseller'
 
 export class AddResellerController implements Controller {
@@ -9,17 +9,21 @@ export class AddResellerController implements Controller {
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const error = this.validation.validate(httpRequest.body)
-    if (error) {
-      return badRequest(error)
+    try {
+      const error = this.validation.validate(httpRequest.body)
+      if (error) {
+        return badRequest(error)
+      }
+      const { socialSecurityNumber, name, email, password } = httpRequest.body
+      await this.addReseller.add({
+        socialSecurityNumber,
+        name,
+        email,
+        password
+      })
+      return Promise.resolve(null)
+    } catch (error) {
+      return serverError(error)
     }
-    const { socialSecurityNumber, name, email, password } = httpRequest.body
-    await this.addReseller.add({
-      socialSecurityNumber,
-      name,
-      email,
-      password
-    })
-    return Promise.resolve(null)
   }
 }
