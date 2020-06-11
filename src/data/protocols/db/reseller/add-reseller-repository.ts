481ea8c06f036f742d8c50a4ebb244ffc43bc52f
AdrianoDeSiqueira <adrianodizao@hotmail.com>
2020-06-11 11:42:50 -0300

    feat: ensure DbAddReseller calls AddResellerRepository with correct values

---
+++ src/data/protocols/db/reseller/add-reseller-repository.ts
@@ -0,0 +1,6 @@
+import { AddResellerModel } from '../../../../domain/usecases/reseller/add-reseller'
+import { ResellerModel } from '../../../../domain/models/reseller'
+
+export interface AddResellerRepository {
+  add: (resellerData: AddResellerModel) => Promise<ResellerModel>
+}
--- src/data/usecases/reseller/db-add-reseller.ts
+++ src/data/usecases/reseller/db-add-reseller.ts
@@ -1,10 +1,14 @@
-import { AddReseller, AddResellerModel, ResellerModel, Hasher } from './db-add-reseller-protocols'
+import { AddReseller, AddResellerModel, ResellerModel, Hasher, AddResellerRepository } from './db-add-reseller-protocols'
 
 export class DbAddReseller implements AddReseller {
-  constructor (private readonly hasher: Hasher) {}
+  constructor (
+    private readonly hasher: Hasher,
+    private readonly addResellerRepository: AddResellerRepository
+  ) {}
 
-  async add (reseller: AddResellerModel): Promise<ResellerModel> {
-    await this.hasher.hash(reseller.password)
+  async add (resellerData: AddResellerModel): Promise<ResellerModel> {
+    const hashedPassword = await this.hasher.hash(resellerData.password)
+    await this.addResellerRepository.add(Object.assign({}, resellerData, { password: hashedPassword }))
     return null
   }
 }
