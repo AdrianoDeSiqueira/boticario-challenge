491cb791fd0be19319ba59683c6b9ca6b44b5b0f
AdrianoDeSiqueira <adrianodizao@hotmail.com>
2020-06-12 11:19:10 -0300

    test: ensure AddOrderController returns 400 if Validation returns an error

--- src/presentation/controllers/order/add-order/add-order-controller.spec.ts
+++ src/presentation/controllers/order/add-order/add-order-controller.spec.ts
@@ -1,5 +1,7 @@
 import { AddOrderController } from './add-order-controller'
 import { HttpRequest, Validation } from './add-order-controller-protocols'
+import { badRequest } from '../../../helpers/http/http-helper'
+import { MissingParamError } from '../../../errors'
 
 const makeValidation = (): Validation => {
   class ValidationStub implements Validation {
@@ -40,5 +42,12 @@ describe('AddOrder Controller', () => {
     const httpRequest = makeFakeRequest()
     await sut.handle(httpRequest)
     expect(validateSpy).toHaveBeenCalledWith(httpRequest.body)
+  })
+
+  test('Should return 400 if Validation returns an error', async () => {
+    const { sut, validationStub } = makeSut()
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new MissingParamError('any_field'))
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('any_field')))
   })
 })
