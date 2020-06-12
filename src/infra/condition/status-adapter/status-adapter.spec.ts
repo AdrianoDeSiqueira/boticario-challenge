d0affc60004249bc96e772f1286c7a00d4a752b0
AdrianoDeSiqueira <adrianodizao@hotmail.com>
2020-06-12 19:40:46 -0300

    test: ensure StatusAdapter return 'Aprovado' for specific social security number

--- src/infra/condition/status-adapter/status-adapter.spec.ts
+++ src/infra/condition/status-adapter/status-adapter.spec.ts
@@ -1,7 +1,9 @@
 import { StatusAdapter } from './status-adapter'
 
+const specificSocialSecurityNumber: string = 'specific_social_security_number'
+
 const makeSut = (): StatusAdapter => {
-  return new StatusAdapter()
+  return new StatusAdapter(specificSocialSecurityNumber)
 }
 
 describe('Status Adapter', () => {
@@ -9,5 +11,11 @@ describe('Status Adapter', () => {
     const sut = makeSut()
     const status = await sut.get('any_social_security_number')
     expect(status).toBe('Em validação')
+  })
+
+  test('Should return "Aprovado" for specific social security number', async () => {
+    const sut = makeSut()
+    const status = await sut.get(specificSocialSecurityNumber)
+    expect(status).toBe('Aprovado')
   })
 })
