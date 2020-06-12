67af7bfc6a43cf25dc6c64724e11f862498f506b
AdrianoDeSiqueira <adrianodizao@hotmail.com>
2020-06-11 23:40:05 -0300

    test: ensure LogControllerDecorator calls controller handle

---
+++ src/main/decorators/log-controller-decorator.spec.ts
@@ -0,0 +1,67 @@
+import { LogControllerDecorator } from './log-controller-decorator'
+import { Controller, HttpRequest, HttpResponse } from '../../presentation/protocols'
+import { created } from '../../presentation/helpers/http/http-helper'
+import { LogErrorRepository } from '../../data/protocols/db/log/log-error-repository'
+import { ResellerModel } from '../../domain/models/reseller'
+
+const makeController = (): Controller => {
+  class ControllerStub implements Controller {
+    async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+      return Promise.resolve(created(makeFakeReseller()))
+    }
+  }
+  return new ControllerStub()
+}
+
+const makeLogErrorRepository = (): LogErrorRepository => {
+  class LogErrorRepositoryStub implements LogErrorRepository {
+    async logError (stack: string): Promise<void> {
+      return Promise.resolve()
+    }
+  }
+  return new LogErrorRepositoryStub()
+}
+
+const makeFakeReseller = (): ResellerModel => ({
+  id: 'valid_id',
+  socialSecurityNumber: 'valid_social_security_number',
+  name: 'valid_name',
+  email: 'valid_email@mail.com',
+  password: 'valid_password'
+})
+
+const makeFakeRequest = (): HttpRequest => ({
+  body: {
+    socialSecurityNumber: 'any_social_security_number',
+    name: 'any_name',
+    email: 'any_email@mail.com',
+    password: 'any_password',
+    passwordConfirmation: 'any_password'
+  }
+})
+
+interface SutTypes {
+  sut: LogControllerDecorator
+  controllerStub: Controller
+  logErrorRepositoryStub: LogErrorRepository
+}
+
+const makeSut = (): SutTypes => {
+  const controllerStub = makeController()
+  const logErrorRepositoryStub = makeLogErrorRepository()
+  const sut = new LogControllerDecorator(controllerStub, logErrorRepositoryStub)
+  return {
+    sut,
+    controllerStub,
+    logErrorRepositoryStub
+  }
+}
+
+describe('LogController Decorator', () => {
+  test('Should call controller handle', async () => {
+    const { sut, controllerStub } = makeSut()
+    const handleSpy = jest.spyOn(controllerStub, 'handle')
+    await sut.handle(makeFakeRequest())
+    expect(handleSpy).toHaveBeenCalledWith(makeFakeRequest())
+  })
+})
