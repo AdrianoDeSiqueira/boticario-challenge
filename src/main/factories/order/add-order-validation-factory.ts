9c6f4527f345a716d0fdf2841214920ea0466232
AdrianoDeSiqueira <adrianodizao@hotmail.com>
2020-06-12 20:54:29 -0300

    feat: ensure AddOrderValidation Factory calls ValidationComposite with all validations

---
+++ src/main/factories/order/add-order-validation-factory.ts
@@ -0,0 +1,10 @@
+import { ValidationComposite, RequiredFieldValidation } from '../../../validation/validators'
+import { Validation } from '../../../presentation/protocols/validation'
+
+export const makeAddOrderValidation = (): ValidationComposite => {
+  const validations: Validation[] = []
+  for (const field of ['code', 'value', 'date', 'socialSecurityNumber']) {
+    validations.push(new RequiredFieldValidation(field))
+  }
+  return new ValidationComposite(validations)
+}
