1e878bdd458ef1b9cbfc4844233d66d26bda73ac
AdrianoDeSiqueira <adrianodizao@hotmail.com>
2020-06-12 19:40:20 -0300

    feat: ensure StatusAdapter return 'Aprovado' for specific social security number

--- src/infra/condition/status-adapter/status-adapter.ts
+++ src/infra/condition/status-adapter/status-adapter.ts
@@ -1,7 +1,12 @@
 import { Status } from '../../../data/protocols/condition/status'
 
 export class StatusAdapter implements Status {
+  constructor (private readonly specificSocialSecurityNumber: string) {}
+
   async get (socialSecurityNumber: string): Promise<string> {
+    if (this.specificSocialSecurityNumber === socialSecurityNumber) {
+      return Promise.resolve('Aprovado')
+    }
     return Promise.resolve('Em validação')
   }
 }
