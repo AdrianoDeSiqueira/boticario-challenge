a1b89e92446df7ad88a5df76c0e39d75728bd4b8
AdrianoDeSiqueira <adrianodizao@hotmail.com>
2020-06-11 22:49:24 -0300

    test: add signup route

---
+++ src/main/routes/reseller-routes.test.ts
@@ -0,0 +1,36 @@
+import request from 'supertest'
+import app from '../config/app'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
+import { Collection } from 'mongodb'
+
+let resellerCollection: Collection
+
+describe('Reseller Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    resellerCollection = await MongoHelper.getCollection('resellers')
+    await resellerCollection.deleteMany({})
+  })
+
+  describe('POST /signup', () => {
+    test('Should return 200 on signup', async () => {
+      await request(app)
+        .post('/api/signup')
+        .send({
+          socialSecurityNumber: 'any_social_security_number',
+          name: 'any_name',
+          email: 'any_email@mail.com',
+          password: 'any_password',
+          passwordConfirmation: 'any_password'
+        })
+        .expect(200)
+    })
+  })
+})
