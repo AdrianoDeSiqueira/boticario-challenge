1a363aea0232b7ff8e7b41ceb981a59e4e2528d5
AdrianoDeSiqueira <adrianodizao@hotmail.com>
2020-06-13 12:22:50 -0300

    test: ensure LoginResellerController returns 400 if Validation returns an error

--- src/presentation/controllers/reseller/login-reseller/login-reseller-controller.spec.ts
+++ src/presentation/controllers/reseller/login-reseller/login-reseller-controller.spec.ts
@@ -1,5 +1,7 @@
 import { LoginResellerController } from './login-reseller-controller'
 import { HttpRequest, Validation } from './login-reseller-controller-protocols'
+import { badRequest } from '../../../helpers/http/http-helper'
+import { MissingParamError } from '../../../errors'
 
 const makeValidation = (): Validation => {
   class ValidationStub implements Validation {
@@ -38,5 +40,12 @@ describe('LoginReseller Controller', () => {
     const httpRequest = makeFakeRequest()
     await sut.handle(httpRequest)
     expect(validateSpy).toHaveBeenCalledWith(httpRequest.body)
+  })
+
+  test('Should return 400 if Validation returns an error', async () => {
+    const { sut, validationStub } = makeSut()
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new MissingParamError('any_field'))
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('any_field')))
   })
 })
