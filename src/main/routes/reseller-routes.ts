e8c5cc0b84f6458c3582bb66c7b060069ee8b4aa
AdrianoDeSiqueira <adrianodizao@hotmail.com>
2020-06-13 18:39:45 -0300

    feat: add login route

--- src/main/routes/reseller-routes.ts
+++ src/main/routes/reseller-routes.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express'
 import { adapterRoute } from '../adapters/express-route-adapter'
 import { makeAddResellerController } from '../factories/reseller/add-reseller/add-reseller-factory'
+import { makeLoginResellerController } from '../factories/reseller/login-reseller/login-reseller-factory'
 
 export default (router: Router): void => {
+  router.post('/login', adapterRoute(makeLoginResellerController()))
   router.post('/reseller', adapterRoute(makeAddResellerController()))
 }
