95164174d1783c89ea92e481662945d6b8e5e79d
AdrianoDeSiqueira <adrianodizao@hotmail.com>
2020-06-13 15:40:54 -0300

    feat: ensure DbAuthentication calls UpdateAccessTokenRepository with correct values

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -1,17 +1,19 @@
-import { Authentication, AuthenticationParams, AuthenticationModel, LoadAccountByEmailRepository, HashComparer, Encrypter } from './db-authentication-protocols'
+import { Authentication, AuthenticationParams, AuthenticationModel, LoadAccountByEmailRepository, HashComparer, Encrypter, UpdateAccessTokenRepository } from './db-authentication-protocols'
 
 export class DbAuthentication implements Authentication {
   constructor (
     private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository,
     private readonly hashComparer: HashComparer,
-    private readonly encrypter: Encrypter
+    private readonly encrypter: Encrypter,
+    private readonly updateAccessTokenRepository: UpdateAccessTokenRepository
   ) {}
 
   async auth (authentication: AuthenticationParams): Promise< AuthenticationModel> {
     const reseller = await this.loadAccountByEmailRepository.loadByEmail(authentication.email)
     if (reseller) {
       await this.hashComparer.compare(authentication.password, reseller.password)
-      await this.encrypter.encrypt(reseller.id)
+      const tokenAccess = await this.encrypter.encrypt(reseller.id)
+      await this.updateAccessTokenRepository.updateAccessToken(reseller.id, tokenAccess)
     }
     return null
   }
