185f6bdd40b86f599287a8d8f61c772f99769e2f
AdrianoDeSiqueira <adrianodizao@hotmail.com>
2020-06-13 12:52:18 -0300

    feat: ensure LoginResellerController returns 500 if Authentications throws

--- src/presentation/controllers/reseller/login-reseller/login-reseller-controller.ts
+++ src/presentation/controllers/reseller/login-reseller/login-reseller-controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Validation, Authentication, HttpRequest, HttpResponse } from './login-reseller-controller-protocols'
-import { badRequest, unauthorized } from '../../../helpers/http/http-helper'
+import { badRequest, unauthorized, serverError } from '../../../helpers/http/http-helper'
 
 export class LoginResellerController implements Controller {
   constructor (
@@ -8,18 +8,22 @@ export class LoginResellerController implements Controller {
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const error = this.validation.validate(httpRequest.body)
-    if (error) {
-      return badRequest(error)
+    try {
+      const error = this.validation.validate(httpRequest.body)
+      if (error) {
+        return badRequest(error)
+      }
+      const { email, password } = httpRequest.body
+      const accessToken = await this.authentication.auth({
+        email,
+        password
+      })
+      if (!accessToken) {
+        return unauthorized()
+      }
+      return Promise.resolve(null)
+    } catch (error) {
+      return serverError(error)
     }
-    const { email, password } = httpRequest.body
-    const accessToken = await this.authentication.auth({
-      email,
-      password
-    })
-    if (!accessToken) {
-      return unauthorized()
-    }
-    return Promise.resolve(null)
   }
 }
