c6f62d976d4e2c399ce5326feb9122075d6cd01f
AdrianoDeSiqueira <adrianodizao@hotmail.com>
2020-06-13 17:27:40 -0300

    test: ensure LoginResellerValidation calls ValidationComposite with all validations

---
+++ src/main/factories/reseller/login-reseller/login-reseller-validation-factory.spec.ts
@@ -0,0 +1,27 @@
+import { makeLoginResellerValidation } from './login-reseller-validation-factory'
+import { ValidationComposite, EmailValidation, RequiredFieldValidation } from '../../../../validation/validators'
+import { Validation } from '../../../../presentation/protocols/validation'
+import { EmailValidator } from '../../../../validation/protocols/email-validator'
+
+jest.mock('../../../../validation/validators/validation-composite')
+
+const makeEmailValidator = (): EmailValidator => {
+  class EmailValidatorStub implements EmailValidator {
+    isValid (email: string): boolean {
+      return true
+    }
+  }
+  return new EmailValidatorStub()
+}
+
+describe('LoginResellerValidation Factory', () => {
+  test('Should call ValidationComposite with all validations', () => {
+    makeLoginResellerValidation()
+    const validations: Validation[] = []
+    for (const field of ['email', 'password']) {
+      validations.push(new RequiredFieldValidation(field))
+    }
+    validations.push(new EmailValidation('email', makeEmailValidator()))
+    expect(ValidationComposite).toHaveBeenCalledWith(validations)
+  })
+})
