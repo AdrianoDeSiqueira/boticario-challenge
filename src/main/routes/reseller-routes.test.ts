35c35a53851aa844b5e582f93fe196d76cdd5142
AdrianoDeSiqueira <adrianodizao@hotmail.com>
2020-06-13 18:41:07 -0300

    test: add login route

--- src/main/routes/reseller-routes.test.ts
+++ src/main/routes/reseller-routes.test.ts
@@ -1,4 +1,5 @@
 import request from 'supertest'
+import { hash } from 'bcrypt'
 import app from '../config/app'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import { Collection } from 'mongodb'
@@ -17,6 +18,34 @@ describe('Reseller Routes', () => {
   beforeEach(async () => {
     resellerCollection = await MongoHelper.getCollection('resellers')
     await resellerCollection.deleteMany({})
+  })
+
+  describe('POST /login', () => {
+    test('Should return 200 on login', async () => {
+      const password = await hash('any_password', 12)
+      await resellerCollection.insertOne({
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password
+      })
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'any_email@mail.com',
+          password: 'any_password'
+        })
+        .expect(200)
+    })
+
+    test('Should return 401 on signup', async () => {
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'any_email@mail.com',
+          password: 'any_password'
+        })
+        .expect(401)
+    })
   })
 
   describe('POST /reseller', () => {
