5ef0db3c74e9aeb9e8f701c82b27c4cfb0f4c413
AdrianoDeSiqueira <adrianodizao@hotmail.com>
2020-06-13 12:48:48 -0300

    test: ensure LoginResellerController returns 401 if invalid credentials are provided

--- src/presentation/controllers/reseller/login-reseller/login-reseller-controller.spec.ts
+++ src/presentation/controllers/reseller/login-reseller/login-reseller-controller.spec.ts
@@ -1,6 +1,6 @@
 import { LoginResellerController } from './login-reseller-controller'
 import { HttpRequest, Validation, Authentication, AuthenticationParams, AuthenticationModel } from './login-reseller-controller-protocols'
-import { badRequest } from '../../../helpers/http/http-helper'
+import { badRequest, unauthorized } from '../../../helpers/http/http-helper'
 import { MissingParamError } from '../../../errors'
 
 const makeValidation = (): Validation => {
@@ -71,5 +71,12 @@ describe('LoginReseller Controller', () => {
       email: 'any_email@mail.com',
       password: 'any_password'
     })
+  })
+
+  test('Should return 401 if invalid credentials are provided', async () => {
+    const { sut, authenticationStub } = makeSut()
+    jest.spyOn(authenticationStub, 'auth').mockReturnValueOnce(Promise.resolve(null))
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(unauthorized())
   })
 })
