c91d8321a395715939c5061c394c6f8c048adefe
AdrianoDeSiqueira <adrianodizao@hotmail.com>
2020-06-15 09:44:08 -0300

    feat: ensure DbLoadOrders calls LoadOrdersRepository with correct social security number

--- src/data/usecases/order/load-orders/db-load-orders.ts
+++ src/data/usecases/order/load-orders/db-load-orders.ts
@@ -1,12 +1,16 @@
-import { LoadResellerByIdRepository, LoadOrders, OrderModel } from './db-load-orders-protocols'
+import { LoadResellerByIdRepository, LoadOrders, OrderModel, LoadOrdersRepository } from './db-load-orders-protocols'
 
 export class DbLoadOrders implements LoadOrders {
   constructor (
-    private readonly loadResellerByIdRepository: LoadResellerByIdRepository
+    private readonly loadResellerByIdRepository: LoadResellerByIdRepository,
+    private readonly loadOrdersRepository: LoadOrdersRepository
   ) {}
 
   async load (resellerId: string): Promise<OrderModel[]> {
-    await this.loadResellerByIdRepository.loadById(resellerId)
+    const reseller = await this.loadResellerByIdRepository.loadById(resellerId)
+    if (reseller) {
+      await this.loadOrdersRepository.loadAll(reseller.socialSecurityNumber)
+    }
     return null
   }
 }
