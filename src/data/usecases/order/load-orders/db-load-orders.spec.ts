1603c731b464e44f6be8f48c62ac77459d22994e
AdrianoDeSiqueira <adrianodizao@hotmail.com>
2020-06-15 01:28:33 -0300

    test: ensure DbLoadOrders calls LoadResellerByIdRepository with correct id

---
+++ src/data/usecases/order/load-orders/db-load-orders.spec.ts
@@ -0,0 +1,44 @@
+import { DbLoadOrders } from './db-load-orders'
+import { LoadResellerByIdRepository } from './db-load-orders-protocols'
+import { ResellerModel } from '../../../../domain/models/reseller'
+
+const makeLoadResellerByIdRepository = (): LoadResellerByIdRepository => {
+  class LoadResellerByIdRepositoryStub implements LoadResellerByIdRepository {
+    async loadById (id: string): Promise<ResellerModel> {
+      return makeFakeReseller()
+    }
+  }
+  return new LoadResellerByIdRepositoryStub()
+}
+
+const makeFakeReseller = (): ResellerModel => ({
+  id: 'any_id',
+  socialSecurityNumber: 'any_social_security_number',
+  name: 'any_name',
+  email: 'any_email@mail.com',
+  password: 'hashed_password'
+})
+
+type SutTypes = {
+  sut: DbLoadOrders
+  loadResellerByIdRepositoryStub: LoadResellerByIdRepository
+}
+
+const makeSut = (): SutTypes => {
+  const loadResellerByIdRepositoryStub = makeLoadResellerByIdRepository()
+  const sut = new DbLoadOrders(loadResellerByIdRepositoryStub)
+  return {
+    sut,
+    loadResellerByIdRepositoryStub
+  }
+}
+
+describe('DbLoadOrders Usecase', () => {
+  test('Should call LoadResellerByIdRepository with correct id', async () => {
+    const { sut, loadResellerByIdRepositoryStub } = makeSut()
+    const loadByIdSpy = jest.spyOn(loadResellerByIdRepositoryStub, 'loadById')
+    const resellerId = 'any_reseller_id'
+    await sut.load(resellerId)
+    expect(loadByIdSpy).toHaveBeenCalledWith(resellerId)
+  })
+})
