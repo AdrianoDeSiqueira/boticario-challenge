da810af7beb01f12e864c90cfe7de052212d92c7
AdrianoDeSiqueira <adrianodizao@hotmail.com>
2020-06-15 11:48:39 -0300

    refactor: group test by function description

--- src/infra/db/mongodb/reseller/reseller-mongo-repository.spec.ts
+++ src/infra/db/mongodb/reseller/reseller-mongo-repository.spec.ts
@@ -4,7 +4,7 @@ import { ResellerMongoRepository } from './reseller-mongo-repository'
 
 let resellerCollection: Collection
 
-describe('Reseller Mongo Repository', () => {
+describe('ResellerMongo Repository', () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL)
   })
@@ -22,58 +22,64 @@ describe('Reseller Mongo Repository', () => {
     return new ResellerMongoRepository()
   }
 
-  test('Should return an reseller on add success', async () => {
-    const sut = makeSut()
-    const reseller = await sut.add({
-      socialSecurityNumber: 'any_social_security_number',
-      name: 'any_name',
-      email: 'any_email@mail.com',
-      password: 'any_password'
+  describe('add()', () => {
+    test('Should return an reseller on add success', async () => {
+      const sut = makeSut()
+      const reseller = await sut.add({
+        socialSecurityNumber: 'any_social_security_number',
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password'
+      })
+      expect(reseller).toBeTruthy()
+      expect(reseller.id).toBeTruthy()
+      expect(reseller.socialSecurityNumber).toBe('any_social_security_number')
+      expect(reseller.name).toBe('any_name')
+      expect(reseller.email).toBe('any_email@mail.com')
+      expect(reseller.password).toBe('any_password')
     })
-    expect(reseller).toBeTruthy()
-    expect(reseller.id).toBeTruthy()
-    expect(reseller.socialSecurityNumber).toBe('any_social_security_number')
-    expect(reseller.name).toBe('any_name')
-    expect(reseller.email).toBe('any_email@mail.com')
-    expect(reseller.password).toBe('any_password')
   })
 
-  test('Should return an reseller on loadByEmail success', async () => {
-    const sut = makeSut()
-    await resellerCollection.insertOne({
-      socialSecurityNumber: 'any_social_security_number',
-      name: 'any_name',
-      email: 'any_email@mail.com',
-      password: 'any_password'
+  describe('loadByEmail()', () => {
+    test('Should return an reseller on loadByEmail success', async () => {
+      const sut = makeSut()
+      await resellerCollection.insertOne({
+        socialSecurityNumber: 'any_social_security_number',
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password'
+      })
+      const reseller = await sut.loadByEmail('any_email@mail.com')
+      expect(reseller).toBeTruthy()
+      expect(reseller.id).toBeTruthy()
+      expect(reseller.socialSecurityNumber).toBe('any_social_security_number')
+      expect(reseller.name).toBe('any_name')
+      expect(reseller.email).toBe('any_email@mail.com')
+      expect(reseller.password).toBe('any_password')
     })
-    const reseller = await sut.loadByEmail('any_email@mail.com')
-    expect(reseller).toBeTruthy()
-    expect(reseller.id).toBeTruthy()
-    expect(reseller.socialSecurityNumber).toBe('any_social_security_number')
-    expect(reseller.name).toBe('any_name')
-    expect(reseller.email).toBe('any_email@mail.com')
-    expect(reseller.password).toBe('any_password')
+
+    test('Should return null if loadByEmail fails', async () => {
+      const sut = makeSut()
+      const reseller = await sut.loadByEmail('any_email@mail.com')
+      expect(reseller).toBeFalsy()
+    })
   })
 
-  test('Should return null if loadByEmail fails', async () => {
-    const sut = makeSut()
-    const reseller = await sut.loadByEmail('any_email@mail.com')
-    expect(reseller).toBeFalsy()
-  })
-
-  test('Should update the account accessToken on updateAccessToken success', async () => {
-    const sut = makeSut()
-    const res = await resellerCollection.insertOne({
-      socialSecurityNumber: 'any_social_security_number',
-      name: 'any_name',
-      email: 'any_email@mail.com',
-      password: 'any_password'
+  describe('updateAccessToken()', () => {
+    test('Should update the account accessToken on updateAccessToken success', async () => {
+      const sut = makeSut()
+      const res = await resellerCollection.insertOne({
+        socialSecurityNumber: 'any_social_security_number',
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password'
+      })
+      const fakeReseller = res.ops[0]
+      expect(fakeReseller.accessToken).toBeFalsy()
+      await sut.updateAccessToken(fakeReseller._id, 'any_token')
+      const reseller = await resellerCollection.findOne({ _id: fakeReseller._id })
+      expect(reseller).toBeTruthy()
+      expect(reseller.accessToken).toBe('any_token')
     })
-    const fakeReseller = res.ops[0]
-    expect(fakeReseller.accessToken).toBeFalsy()
-    await sut.updateAccessToken(fakeReseller._id, 'any_token')
-    const reseller = await resellerCollection.findOne({ _id: fakeReseller._id })
-    expect(reseller).toBeTruthy()
-    expect(reseller.accessToken).toBe('any_token')
   })
 })
