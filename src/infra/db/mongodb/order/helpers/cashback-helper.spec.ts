10f89843cae26cbf22db2b8b541afd5cdf7bcf80
AdrianoDeSiqueira <adrianodizao@hotmail.com>
2020-06-15 22:33:19 -0300

    test: ensure CashbackHelper return 15% percentage

--- src/infra/db/mongodb/order/helpers/cashback-helper.spec.ts
+++ src/infra/db/mongodb/order/helpers/cashback-helper.spec.ts
@@ -10,4 +10,10 @@ describe('Cashback Helper', () => {
     const cashback = await sut.get(989.90)
     expect(cashback).toEqual({ perc: 10, value: 98.99 })
   })
+
+  test('Should return 15% percentage', async () => {
+    const sut = makeSut()
+    const cashback = await sut.get(1245.90)
+    expect(cashback).toEqual({ perc: 15, value: 186.885 })
+  })
 })
