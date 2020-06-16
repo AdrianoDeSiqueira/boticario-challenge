12923dbe5f36e29651b0b935640956210cf5a938
AdrianoDeSiqueira <adrianodizao@hotmail.com>
2020-06-15 22:31:53 -0300

    feat: ensure CashbackHelper return 15% percentage

--- src/infra/db/mongodb/order/helpers/cashback-helper.ts
+++ src/infra/db/mongodb/order/helpers/cashback-helper.ts
@@ -2,11 +2,11 @@ import { GetCashbackHelper } from '@/data/protocols/db/order/helpers/get-cashbac
 
 export class CashbackHelper implements GetCashbackHelper {
   async get (valuePurchase: number): Promise<object> {
-    let perc: number
-    if (valuePurchase <= 1000) {
-      perc = 10
+    let perc = 10
+    if (valuePurchase > 1000 && valuePurchase <= 1550) {
+      perc = 15
     }
-    const value = (perc * valuePurchase) / 100
+    const value = ((perc * valuePurchase) / 100)
     return {
       perc,
       value
