267b806018c1b6ede0a90eba2bb379fc14de286f
AdrianoDeSiqueira <adrianodizao@hotmail.com>
2020-06-16 15:34:29 -0300

    test: ensure CashbackApi calls request get with correct values

--- src/infra/external/cashback-api/cashback-api.spec.ts
+++ src/infra/external/cashback-api/cashback-api.spec.ts
@@ -1,10 +1,39 @@
 import { CashbackApi } from './cashback-api'
+import request from 'request-promise-native'
+import env from '@/main/config/env'
+
+jest.mock('request-promise-native', () => ({
+  async get (): Promise<string> {
+    const res = {
+      statusCode: 200,
+      body: {
+        credit: 1000
+      }
+    }
+    return Promise.resolve(JSON.stringify(res))
+  }
+}))
+
+const fakeRequestOptions = (): object => ({
+  uri: env.cashbackApi + '?cpf=15350946056',
+  headers: {
+    contentType: 'application/json',
+    token: env.cashbackApiToken
+  }
+})
 
 const makeSut = (): CashbackApi => {
   return new CashbackApi()
 }
 
 describe('Cashback Api', () => {
+  test('Should call get with correct values', async () => {
+    const sut = makeSut()
+    const getSpy = jest.spyOn(request, 'get')
+    await sut.load('15350946056')
+    expect(getSpy).toHaveBeenCalledWith(fakeRequestOptions())
+  })
+
   test('Should return an cashback on load success', async () => {
     const sut = makeSut()
     const cashback = await sut.load('15350946056')
