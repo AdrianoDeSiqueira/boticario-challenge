39d658522f1bb79f6af615d9d450ca4ea35b93b6
AdrianoDeSiqueira <adrianodizao@hotmail.com>
2020-06-16 21:29:56 -0300

    feat: ensure ITRFieldValidation not return if validation succeeds

--- src/validation/validators/itr-field-validation.ts
+++ src/validation/validators/itr-field-validation.ts
@@ -2,9 +2,14 @@ import { Validation } from '@/presentation/protocols/validation'
 import { InvalidParamError } from '@/presentation/errors/invalid-param-error'
 
 export class ITRFieldValidation implements Validation {
-  constructor (private readonly fieldName: string) {}
+  constructor (
+    private readonly fieldName: string
+  ) {}
 
   validate (input: any): Error {
-    return new InvalidParamError(this.fieldName)
+    // eslint-disable-next-line no-useless-escape
+    if (!/^\d{3}\.\d{3}\.\d{3}\-\d{2}$/.test(input[this.fieldName])) {
+      return new InvalidParamError(this.fieldName)
+    }
   }
 }
